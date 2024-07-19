2b2160378f83397bfa1ca07a2458381b26d5be70
Seiji Kamiya <10seiji18@gmail.com>
2024-07-19 14:09:32 +0900

    Add df_to_sql function in db_manage.py

--- db_manage.py
+++ db_manage.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import sqlite3
+import pandas as pd
 from getyfin import GetYfin
 from get_weather import GetWeather
 
@@ -12,21 +13,18 @@ class BaseDB(ABC):
     def create_db():
         pass
 
-    # @abstractmethod
-    # def insert_data():
-    #     pass
-
 class WeatherDB(BaseDB):
     def create_db(self):
         api_key = 'WGPLJP357LBQE7KGMJ4MBG4GS'
         get_weather = GetWeather(api_key)
         df = get_weather.fetch_data()
+        df.index.name = 'WeatherID'
 
         # SQLiteデータベースに接続（新しいデータベースを作成）
         conn = sqlite3.connect(self.path)
 
         # DataFrameをデータベースに書き込む
-        df.to_sql(self.table_name, conn, index=False, if_exists='replace')
+        df.to_sql(self.table_name, conn, index=True, if_exists='replace')
 
         # コミットして接続を閉じる
         conn.commit()
@@ -36,35 +34,51 @@ class StockDB(BaseDB):
     def create_db(self):
         ticker = '9201.T'  # 企業コード
         get_yfin = GetYfin(ticker)
-        df = get_yfin.fetch_data()
+        df = get_yfin.data_pipeline()
+        df.index.name = 'StockID'
 
         # SQLiteデータベースに接続（新しいデータベースを作成）
         conn = sqlite3.connect(self.path)
 
         # DataFrameをデータベースに書き込む
-        df.to_sql(self.table_name, conn, index=False, if_exists='replace')
+        df.to_sql(self.table_name, conn, index=True, if_exists='replace')
 
         # コミットして接続を閉じる
         conn.commit()
         conn.close()
 
 
-def sql_query():
+def sql_query(path, query):
     # データベースファイルのパス
-    db_path = "stock_weather.db"
+    db_path = path
 
     # SQLiteデータベースに接続
     conn = sqlite3.connect(db_path)
 
     # カーソルの作成
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM weather")
-    weather_data = cursor.fetchall()
+    df = pd.read_sql_query(query, conn)
 
-    return weather_data
+    return df
+
+def df_to_sql(df, path, table_name):
+        conn = sqlite3.connect(path)
+        df.index.name = 'WeatherEffectID'
+        df.to_sql(table_name, conn, index=True, if_exists='replace')
+
+        # コミットして接続を閉じる
+        conn.commit()
+        conn.close()
 
 if __name__ == "__main__":
-    stock_db = StockDB("example.db", "stock")
-    stock_db.create_db()
-    weather_db = WeatherDB("weather.db", "weather")
-    weather_db.create_db()
+    # stock_db = StockDB("stock_weather.db", "stock")
+    # stock_db.create_db()
+    # weather_db = WeatherDB("stock_weather.db", "weather")
+    # weather_db.create_db()
+    query = """
+    SELECT weather_description, AVG(Diff) as PriceDiff FROM stock
+    INNER JOIN weather
+    ON stock.date == weather.Timestamp
+    GROUP BY weather_description
+    """
+    df = sql_query("stock_weather.db", query)
+    df_to_sql(df, "stock_weather.db", "WeatherEffect")
